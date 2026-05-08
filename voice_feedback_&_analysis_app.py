e873059fec16704152d71f97f6e4e0fdb26e772d
shrishail07 <144917838+shrishail07@users.noreply.github.com>
2026-05-08 12:26:34 +0530

    Update voice_feedback_&_analysis_app.py

--- voice_feedback_&_analysis_app.py
+++ voice_feedback_&_analysis_app.py
@@ -14,7 +14,11 @@ from openpyxl import Workbook, load_workbook
 # --- PAGE CONFIGURATION ---
 st.set_page_config(page_title="Student Voice Feedback System", page_icon="🎤", layout="wide")
 
-st.image("pragyan_ai_school_cover.jpg", width=1200)
+# st.image("pragyan_ai_school_cover.jpg", width=1200)
+col1, col2, col3 = st.columns([1, 4, 1]) 
+
+with col2:
+    st.image("pragyan_ai_school_cover.jpg", use_container_width=True)
 
 # --- PASSWORD CONFIGURATION ---
 ADMIN_PASSWORD = "PRAGYANAI"
